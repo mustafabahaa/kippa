b8240e3674c64a102dfac568654804d63a0298a7
Mustafa BAHAA <mustafa.bahaa123@gmail.com>
2026-06-29 22:46:42 +0300

    feat(functions): add onTransactionCreated trigger (push + category warning)

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,3 +1,7 @@
 import { initializeApp } from 'firebase-admin/app';
 
 initializeApp();
+
+export { onTransactionCreated } from './onTransactionCreated.js';
+export { dailyReminderCron } from './dailyReminderCron.js';
+export { cycleCloseCron } from './cycleCloseCron.js';
